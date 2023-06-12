5ec145923db42b23045d84e206ae1be610a3bc20
Poladdhuseynov <135529883+Poladdhuseynov@users.noreply.github.com>
2023-06-12 18:30:22 +0400

    HACKER.py
    Hacker.py

---
+++ Hacker.kurulum.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python 3
+import os
+
+os.system("clear")
+os.system("pkg install figlet")
+os.system("clear")
+os.system("figlet kurulum basliyor")
+os.system("pkg install nmap")
+os.system("pkg install whois")
+os.system("pkg install zphisher")
+os.system("git clone https://github.com/sqlmapproject/sqlmap")
+os.system("clear")
+os.system("figlet kurulum bitti")
+
+print("""
+******************************************************
+
+tool hazir vəzyətdə:)
+
+""")
